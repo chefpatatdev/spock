e85b56f8d38183d5924bbf82ff9b01d603255326
chefpatat <dasilvamarquesricky@gmail.com>
2023-10-29 17:56:15 +0100

    nu wel correct nieuw project, vanaf hier branchen

---
+++ HomeScreen.cs
@@ -0,0 +1,24 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SpockApp.src
+{
+    [Activity(Label = "HomeScreen")]
+    public class HomeScreen : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.home_screen);
+            // Create your application here
+        }
+    }
+}
---
+++ LoginScreen.cs
@@ -0,0 +1,81 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using EncryptionDecryptionUsingSymmetricKey;
+using Google.Android.Material.Snackbar;
+using SpockApp.src;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace SpockApp.Resources
+{
+    [Activity(Label = "LoginScreen", MainLauncher = true)]
+    public class LoginScreen : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.login_screen);
+            // Create your application here
+            Button myButton = FindViewById<Button>(Resource.Id.login_button);
+            myButton.Click += LoginAttempt_Click;
+
+
+        }
+
+
+        
+        private void LoginAttempt_Click(object sender, System.EventArgs e)
+        {
+            // Code to be executed when the button is clicked
+            
+            EditText usernameField = FindViewById<EditText>(Resource.Id.user_name_field);
+            string usernameRaw = usernameField.Text;
+
+            EditText passwordField = FindViewById<EditText>(Resource.Id.password_field);
+            string passwordRaw = passwordField.Text;
+            Console.WriteLine(usernameRaw);
+            Console.WriteLine(passwordRaw);
+
+            bool allowedEntrance = false;
+
+            //temporary for development reasons
+            if(usernameRaw.Equals("admin") && passwordRaw.Equals("admin"))
+            {
+                allowedEntrance = true;
+            }
+
+
+            var key = "b14ca5898a4e4133bbce2ea2315a1916";
+            var encryptedUsername = AesOperation.EncryptString(key, usernameRaw);
+            var encryptedPassword = AesOperation.EncryptString(key, passwordRaw);
+            Console.WriteLine(encryptedUsername);
+            Console.WriteLine(encryptedPassword);
+            //encrypt password before sending message thru socket
+
+            //allowedEntrance = response from socket
+
+            if (allowedEntrance)
+            {
+                //swicth to homescreen
+                Intent intent = new Intent(this, typeof(HomeScreen));
+                StartActivity(intent);
+            }
+            else
+            {
+                View view = (View)sender;
+                Snackbar.Make(view, "Wrong password and/or username!", Snackbar.LengthLong)
+                    .SetAction("Action", (Android.Views.View.IOnClickListener)null).Show();
+            }
+            
+
+
+        }
+    }
+}
