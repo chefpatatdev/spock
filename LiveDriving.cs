c889f8b63296dcb2fcaa7de5d65406bc346891c1
chefpatat <dasilvamarquesricky@gmail.com>
2023-11-05 16:36:03 +0100

    button press and release + update buttons

--- LiveDriving.cs
+++ LiveDriving.cs
@@ -20,6 +20,87 @@ namespace SpockApp.Resources.mipmap_xhdpi
             SetContentView(Resource.Layout.live_driving);
 
             // Create your application here
+            Button upButton = FindViewById<Button>(Resource.Id.up_button);
+            upButton.Touch += UpButton_Touch;
+
+            Button downButton = FindViewById<Button>(Resource.Id.down_button);
+            downButton.Touch += DownButton_Touch;
+
+            Button rightButton = FindViewById<Button>(Resource.Id.right_button);
+            rightButton.Touch += RightButton_Touch;
+
+            Button leftButton = FindViewById<Button>(Resource.Id.left_button);
+            leftButton.Touch += LeftButton_Touch;
+
         }
+
+        private void UpButton_Touch(object sender, View.TouchEventArgs e)
+        { 
+            switch (e.Event.Action & MotionEventActions.Mask)
+            {
+                case MotionEventActions.Down:
+                case MotionEventActions.Move://treated same as down action
+                    Console.WriteLine("up button pressed");
+                    break;
+                case MotionEventActions.Up:
+                    Console.WriteLine("up button released");
+                    break;
+                default:
+                    Console.WriteLine("default up");
+                    break;
+            }
+        }
+
+        private void DownButton_Touch(object sender, View.TouchEventArgs e)
+        {
+            switch (e.Event.Action & MotionEventActions.Mask)
+            {
+                case MotionEventActions.Down:
+                case MotionEventActions.Move:
+                    Console.WriteLine("down button pressed");
+                    break;
+                case MotionEventActions.Up:
+                    Console.WriteLine("down button released");
+                    break;
+                default:
+                    Console.WriteLine("default down");
+                    break;
+            }
+        }
+
+        private void RightButton_Touch(object sender, View.TouchEventArgs e)
+        {
+            switch (e.Event.Action & MotionEventActions.Mask)
+            {
+                case MotionEventActions.Down:
+                case MotionEventActions.Move: 
+                    Console.WriteLine("right button pressed");
+                    break;
+                case MotionEventActions.Up:
+                    Console.WriteLine("right button released");
+                    break;
+                default:
+                    Console.WriteLine("default right");
+                    break;
+            }
+        }
+
+        private void LeftButton_Touch(object sender, View.TouchEventArgs e)
+        {
+            switch (e.Event.Action & MotionEventActions.Mask)
+            {
+                case MotionEventActions.Down:
+                case MotionEventActions.Move:
+                    Console.WriteLine("left button pressed");
+                    break;
+                case MotionEventActions.Up:
+                    Console.WriteLine("left button released");
+                    break;
+                default:
+                    Console.WriteLine("default left");
+                    break;
+            }
+        }
+
     }
 }
