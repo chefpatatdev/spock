01ed0e2a6c3e5a3189ca8ab1d1299b53056b0fd5
Emmelie12 <emmelie.desaeyer@student.kuleuven.be>
2023-11-12 22:18:21 +0100

    add measuring screen

---
+++ MeasuringScreen.cs
@@ -0,0 +1,31 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+
+namespace SpockApp.Resources
+
+
+{
+    [Activity(Label = "MeasuringScreen")]
+    public class MeasuringScreen : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.measuring_screen);
+            // Create your application here
+            Button measureButton = FindViewById<Button>(Resource.Id.measure_button);
+            //LoginButton.Click += LoginAttempt_Click;
+
+
+        }
+    }
+}
