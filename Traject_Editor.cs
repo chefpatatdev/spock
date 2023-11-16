78f95f4e51db620594b2a1b1e940c5764ff5e056
KingThyby <thybo.bogaert5@gmail.com>
2023-11-16 17:23:25 +0100

    added comentaar

--- Traject_Editor.cs
+++ Traject_Editor.cs
@@ -22,31 +22,26 @@ namespace SpockApp
         static ListViewAdapter adapter;
         static Context context;
         static EditText input;
-
-
-
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.traject_editor);
 
             Trajects = Intent.GetStringArrayExtra("arrayID");
+            context = ApplicationContext;
+            adapter = new ListViewAdapter(this, Trajects);
 
             Button btn = FindViewById<Button>(Resource.Id.add_traject);
-
-            btn.Click += Add_Click;
-            // Create your application here
             list = (ListView)FindViewById<ListView>(Resource.Id.listview_edit);
-            adapter = new ListViewAdapter(this, Trajects);
-            list.Adapter = adapter;
-            list.ItemClick += new EventHandler<AdapterView.ItemClickEventArgs>(Listview_Click);
-
-            context = ApplicationContext;
             input = FindViewById<EditText>(Resource.Id.traject_input);
 
+            btn.Click += Add_Click;
+            list.ItemClick += new EventHandler<AdapterView.ItemClickEventArgs>(Listview_Click);
+            list.Adapter = adapter;
         }
         public override void OnBackPressed()
         {
+            //als terug gaat naar traject_driving wordt de veranderde stringarray meegegeven zodat daar gebruikt kan worden
             Intent intent = new Intent(this, typeof(TrajectDriving));
             intent.PutExtra("trajectID", Trajects);
             StartActivity(intent);
@@ -54,54 +49,55 @@ namespace SpockApp
 
         private void Add_Click(object sender, EventArgs e)
         {
+            //voegt de ingevoerde string toe aan de projecten en kijkt ook ofdat deze niet leeg is
             string text = input.Text.ToString();
             if (string.IsNullOrEmpty(text))
             {
                 Toast.MakeText(this, "Enter Something", ToastLength.Short).Show();
-                InitializeListView();
+                UpdateListView();
             }
             else {
                 AddItems(text);
                 input.Text = "";
             }
-            
         }
 
-        private void InitializeListView()
+        private static void UpdateListView()
         {
-            //Maken van drop down menu om traject te slecteren
-            list = FindViewById<ListView>(Resource.Id.listview_edit);
-            adapter = new ListViewAdapter(this, Trajects);
+            //update listview adapter
+            adapter = new ListViewAdapter(context, Trajects);
             list.Adapter = adapter;
-            list.ItemClick += new EventHandler<AdapterView.ItemClickEventArgs>(Listview_Click);
 
         }
 
         private void Listview_Click(object sender, AdapterView.ItemClickEventArgs e)
-        {
+        {   
+            //eventuele click handler als er iets moet gebeuren als er op een item geklikt wordt
             var list = (ListView)sender;
             Toast.MakeText(this, list.GetItemAtPosition(e.Position).ToString(), ToastLength.Short).Show();
         }
 
         private void AddItems(string item)
         {
-            var list = new List<string>();
-            list = Trajects.ToList();
-            list.Add(item);
-            Trajects = list.ToArray();
-            InitializeListView();
+            // item aan trajecten toevoegen door naar een modifiable lijst om te zetten
+            var tmplist = new List<string>(Trajects.ToList())
+            {
+                item
+            };
+            Trajects = tmplist.ToArray();
+            UpdateListView();
         }
         public static void RemoveItems(string item)
         {
+            // items van trajecten verwijderen
             Trajects = Trajects.Where(o => o != item).ToArray();
-
-            adapter = new ListViewAdapter(context, Trajects);
-            list.Adapter = adapter;
+            UpdateListView();
         }
         public static void ModifyItems(string item)
-        {
-
+        {   
+            // namen van trajecten wijzigen met wat er in het tekstvak is ingetypt
             string text = input.Text.ToString();
+
             if (string.IsNullOrEmpty(text))
             {
                 Toast.MakeText(context, "Enter Something", ToastLength.Short).Show();
@@ -111,8 +107,7 @@ namespace SpockApp
                 Trajects[Array.FindIndex(Trajects, m => m == item)] = text;
                 input.Text = "";
             }
-            adapter = new ListViewAdapter(context, Trajects);
-            list.Adapter = adapter;
+            UpdateListView();
         }
     }
 }
