43a6a24e769bbe56d60b7836bf68753a05c4b35f
KingThyby <thybo.bogaert5@gmail.com>
2023-11-21 14:59:09 +0100

    Changed App Icon

--- Resources/Resource.designer.cs
+++ Resources/Resource.designer.cs
@@ -4607,13 +4607,16 @@ namespace SpockApp
 		{
 			
 			// aapt resource value: 0x7F0D0000
-			public const int ic_launcher = 2131558400;
+			public const int app_icon_foreground = 2131558400;
 			
 			// aapt resource value: 0x7F0D0001
-			public const int ic_launcher_foreground = 2131558401;
+			public const int ic_launcher = 2131558401;
 			
 			// aapt resource value: 0x7F0D0002
-			public const int ic_launcher_round = 2131558402;
+			public const int ic_launcher_foreground = 2131558402;
+			
+			// aapt resource value: 0x7F0D0003
+			public const int ic_launcher_round = 2131558403;
 			
 			static Mipmap()
 			{
