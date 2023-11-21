fc588a4ffb2c2f6670dce04c68c9958aea045408
KingThyby <thybo.bogaert5@gmail.com>
2023-11-21 16:28:55 +0100

    adapter made + layout changes

---
+++ ListViewAdapterMeasure.cs
@@ -0,0 +1,47 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SpockApp
+{
+    class ListViewAdapterMeasure : ArrayAdapter<string>
+    {
+        string[] list;
+        Context context;
+        public ListViewAdapterMeasure(Context context, string[] items) : base(context, Resource.Layout.list_row, items)
+        {
+            this.context = context;
+            list = items;
+        }
+        public override View GetView(int position, View convertView, ViewGroup parent)
+        {
+            if(convertView == null)
+            {
+                LayoutInflater lf = (LayoutInflater)context.GetSystemService(Activity.LayoutInflaterService);
+                convertView = lf.Inflate(Resource.Layout.list_row_measure, null);
+
+                TextView value = convertView.FindViewById<TextView>(Resource.Id.textView1);
+                value.Text = list[position];
+
+                TextView scalar = convertView.FindViewById<TextView>(Resource.Id.textView2);
+                scalar.Text = list[position];
+
+                TextView sensor = convertView.FindViewById<TextView>(Resource.Id.textView3);
+                sensor.Text = list[position];
+
+                TextView timestamp = convertView.FindViewById<TextView>(Resource.Id.textView4);
+                timestamp.Text = list[position];
+
+            }
+            return base.GetView(position, convertView, parent);
+        }
+
+    }
+}
