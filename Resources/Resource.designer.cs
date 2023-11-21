ed9eb53978b779ebe145a57abf0a26815c7fa920
KingThyby <thybo.bogaert5@gmail.com>
2023-11-21 13:44:08 +0100

    Added files

--- Resources/Resource.designer.cs
+++ Resources/Resource.designer.cs
@@ -4543,52 +4543,55 @@ namespace SpockApp
 			public const int login_screen = 2131492912;
 			
 			// aapt resource value: 0x7F0C0031
-			public const int measuring_screen = 2131492913;
+			public const int manual_measurement_screen = 2131492913;
 			
 			// aapt resource value: 0x7F0C0032
-			public const int mtrl_layout_snackbar = 2131492914;
+			public const int measuring_screen = 2131492914;
 			
 			// aapt resource value: 0x7F0C0033
-			public const int mtrl_layout_snackbar_include = 2131492915;
+			public const int mtrl_layout_snackbar = 2131492915;
 			
 			// aapt resource value: 0x7F0C0034
-			public const int notification_action = 2131492916;
+			public const int mtrl_layout_snackbar_include = 2131492916;
 			
 			// aapt resource value: 0x7F0C0035
-			public const int notification_action_tombstone = 2131492917;
+			public const int notification_action = 2131492917;
 			
 			// aapt resource value: 0x7F0C0036
-			public const int notification_template_custom_big = 2131492918;
+			public const int notification_action_tombstone = 2131492918;
 			
 			// aapt resource value: 0x7F0C0037
-			public const int notification_template_icon_group = 2131492919;
+			public const int notification_template_custom_big = 2131492919;
 			
 			// aapt resource value: 0x7F0C0038
-			public const int notification_template_part_chronometer = 2131492920;
+			public const int notification_template_icon_group = 2131492920;
 			
 			// aapt resource value: 0x7F0C0039
-			public const int notification_template_part_time = 2131492921;
+			public const int notification_template_part_chronometer = 2131492921;
 			
 			// aapt resource value: 0x7F0C003A
-			public const int pickertest = 2131492922;
+			public const int notification_template_part_time = 2131492922;
 			
 			// aapt resource value: 0x7F0C003B
-			public const int select_dialog_item_material = 2131492923;
+			public const int pickertest = 2131492923;
 			
 			// aapt resource value: 0x7F0C003C
-			public const int select_dialog_multichoice_material = 2131492924;
+			public const int select_dialog_item_material = 2131492924;
 			
 			// aapt resource value: 0x7F0C003D
-			public const int select_dialog_singlechoice_material = 2131492925;
+			public const int select_dialog_multichoice_material = 2131492925;
 			
 			// aapt resource value: 0x7F0C003E
-			public const int support_simple_spinner_dropdown_item = 2131492926;
+			public const int select_dialog_singlechoice_material = 2131492926;
 			
 			// aapt resource value: 0x7F0C003F
-			public const int traject_driving = 2131492927;
+			public const int support_simple_spinner_dropdown_item = 2131492927;
 			
 			// aapt resource value: 0x7F0C0040
-			public const int traject_editor = 2131492928;
+			public const int traject_driving = 2131492928;
+			
+			// aapt resource value: 0x7F0C0041
+			public const int traject_editor = 2131492929;
 			
 			static Layout()
 			{
